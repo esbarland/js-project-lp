09a45b2cc380f4ba93ed22b41c908eb77119d8bf
esbarland <esbarland@etu.uca.fr>
2020-01-10 15:19:10 +0100

    ajout des 5 méthodes dans le client et écriture des tests pour les 5 méthodes implémentées

--- packages/http-client/lib/client.js
+++ packages/http-client/lib/client.js
@@ -1,24 +1,91 @@
 module.exports = function(host, port) {
     
-    function getAll() {
+    /**
+     * Request Get all cars
+     */
+    function findAll() {
+        var options =  {
+            method: 'get'
+        };
+
         return new Promise((resolve, reject) => {
-            fetch(`http://${host}:${port}/api/change`)
+            fetch(`http://${host}:${port}/api/cars`, options)
                 .then((response) => {
                     resolve(response.json());
                 })
                 .catch(reject)
         });
     }
-    function add() {
+
+    /**
+     * Request get one car
+     */
+    function findOne(){
+        var options =  {
+            method: 'get',
+        };
+
         return new Promise((resolve, reject) => {
-            fetch('http://localhost:7890/api/change', {
-            method: 'post',
-            body: JSON.stringify({eee: "uuuuu"})
-        }).then(resolve).catch(reject)
+            fetch(`http://${host}:${port}/api/cars`, options)
+                .then((response) => {
+                    resolve(response.json());
+                })
+                .catch(reject)
         });
     }
+
+    /**
+     * Request Post add a new car
+     */
+    function add() {
+        var options =  {
+            method: 'post',
+            body: JSON.stringify({eee: "uuuuu"})
+        };
+
+        return new Promise((resolve, reject) => {
+            fetch(`http://${host}:${port}/api/cars`, options)
+            .then(resolve).catch(reject)
+        });
+    }
+
+    /**
+     * Request Delete remove a car
+     */
+    function remove() {
+        var options =  {
+            method: 'delete',
+            body: JSON.stringify({eee: "uuuuu"})
+        };
+
+        return new Promise((resolve, reject) => {
+            fetch(`http://${host}:${port}/api/cars`, options)
+            .then(resolve).catch(reject)
+        });
+    }
+
+    /**
+     * Request Put update a car
+     */
+    function update(){
+        var options =  {
+            method: 'put',
+            body: JSON.stringify({eee: "uuuuu"})
+        };
+
+        return new Promise((resolve, reject) => {
+            fetch(`http://${host}:${port}/api/cars`, options)
+            .then(resolve).catch(reject)
+        });
+    }
+
+
+
     return {
-        getAll: getAll,
-        add: add
+        findAll: findAll,
+        findOne: findOne,
+        add: add,
+        remove: remove,
+        update: update,
     }
 }
--- packages/http-client/lib/tests/index.js
+++ packages/http-client/lib/tests/index.js
@@ -10,27 +10,66 @@ const httpclient = require('../client')
 
 const { expect } = chai;
 
-describe('Test API', () => {
+const server_address = 'localhost';
+const server_port = 7890;
+
+describe('Test API Cars', () => {
 
     afterEach(fetchMock.resetBehavior)
 
-    describe('Test Get', () => {
+    describe('Test Get all cars', () => {
         it('test /', async () => {
-            fetchMock.get(`http://localhost:7890/api/change`, [{attribute_a: 'AAA', attribute_b: 'BBB', attribute_c: 333}]);
-            const client = httpclient('localhost', 7890);
-            client.getAll().then((changes) => {
+            fetchMock.get(`http://${server_address}:${server_port}/api/cars`, [{attribute_a: 'AAA', attribute_b: 'BBB', attribute_c: 333}]);
+            const client = httpclient(server_address, server_port);
+            client.findAll().then((changes) => {
                 expect(changes).to.eql([{attribute_a: 'AAA', attribute_b: 'BBB', attribute_c: 333}]);
+                const request = fetchMock.lastCall()[1];
+                expect(request.method).equal('get');
             })
         });
     });
 
-    describe('Test Post', () => {
+    describe('Test Get one car', () => {
         it('test /', async () => {
-            fetchMock.post(`http://localhost:7890/api/change`, {eeee: "tttt"});
-            const client = httpclient('localhost', 7890);
+            fetchMock.get(`http://${server_address}:${server_port}/api/cars`, [{attribute_a: 'AAA', attribute_b: 'BBB', attribute_c: 333}]);
+            const client = httpclient(server_address, server_port);
+            client.findOne().then((changes) => {
+                expect(changes).to.eql([{attribute_a: 'AAA', attribute_b: 'BBB', attribute_c: 333}]);
+                const request = fetchMock.lastCall()[1];
+                expect(request.method).equal('get');
+            })
+        });
+    });
+
+    describe('Test Post add car', () => {
+        it('test /', async () => {
+            fetchMock.post(`http://${server_address}:${server_port}/api/cars`, {eeee: "tttt"});
+            const client = httpclient(server_address, server_port);
             client.add().then((changes) => {
                 const request = fetchMock.lastCall()[1];
                 expect(request.method).equal('post');
+            })
+        });
+    });
+
+    describe('Test Delete remove a car', () => {
+        it('test /', async () => {
+            fetchMock.delete(`http://${server_address}:${server_port}/api/cars`, {eeee: "tttt"});
+            const client = httpclient(server_address, server_port);
+            client.remove().then((changes) => {
+                const request = fetchMock.lastCall()[1];
+                expect(request.method).equal('delete');
+            })
+        });
+    });
+
+    describe('Test Put update a car', () => {
+        it('test /', async () => {
+            fetchMock.put(`http://${server_address}:${server_port}/api/cars`, {eeee: "tttt"});
+            const client = httpclient(server_address, server_port);
+            client.update().then((changes) => {
+                const request = fetchMock.lastCall()[1];
+                expect(request.method).equal('put');
             })
         });
     });
