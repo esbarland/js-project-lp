9145aaba6a0eb905475dd7d082fd4e890156b6be
esbarland <esbarland@etu.uca.fr>
2020-01-24 15:40:29 +0100

    début du backend

--- backend/index.js
+++ backend/index.js
@@ -16,8 +16,47 @@ app.get('/api/database/test', (req, res) => {
       })
     pool.query('SELECT NOW()', (err, result) => {
         pool.end()
-        res.send(`Il est ${result.rows[0].now}`)
+        res.send(`Il est ${result.rows[0].now} salut la modif`)
     })
-})
+});
+
+/**
+ * GET All cars
+ */
+app.get('/api/cars', (req, res) =>{
+    const pool = new Pool({
+        user: 'db_user',
+        host: 'database',
+        database: 'db_db',
+        password: 'db_password'
+      })
+      pool.query('SELECT * FROM cars', (err, result) => {
+        pool.end();
+        
+
+        var cars = [];
+        for(var key in result.rows){
+            cars.push(result.rows[key]);
+        }
+
+        res.send(cars);
+      })
+});
+
+/**
+ * GET one car
+ */
+/*app.get('/api/cars/{id}', (req, res) =>{
+    const pool = new Pool({
+        user: 'db_user',
+        host: 'database',
+        database: 'db_db',
+        password: 'db_password'
+      })
+      pool.query('SELECT * FROM cars WHERE cars.id = $1', id, (err, result) => {
+          pool.end();
+          res.send(`${result}`);
+      })
+});*/
 
 module.exports = app;
