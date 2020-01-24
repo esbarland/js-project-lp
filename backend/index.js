89017a95d0473a37f987f3d9d3f7415b73119da6
esbarland <esbarland@etu.uca.fr>
2020-01-24 17:27:19 +0100

    get all, get, put, delete and post are working, need errors status

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,14 @@
+const uuidv4 = require('uuid/v4');
 const express = require('express');
 const morgan = require('morgan');
+var bodyParser = require('body-parser')
 
 const { Pool } = require('pg')
 
 const app = express();
 
 app.use(morgan('dev'));
+app.use(bodyParser.json())
 
 app.get('/api/database/test', (req, res) => {
     const pool = new Pool({
@@ -46,17 +49,100 @@ app.get('/api/cars', (req, res) =>{
 /**
  * GET one car
  */
-/*app.get('/api/cars/{id}', (req, res) =>{
+app.get('/api/cars/:id', (req, res) =>{
     const pool = new Pool({
         user: 'db_user',
         host: 'database',
         database: 'db_db',
         password: 'db_password'
       })
-      pool.query('SELECT * FROM cars WHERE cars.id = $1', id, (err, result) => {
-          pool.end();
-          res.send(`${result}`);
+
+      var id = req.params.id;
+      var car;
+
+      pool.query('SELECT * FROM cars WHERE cars.id = $1', [id], (err, result) => {
+        pool.end();
+        if(err == null){
+          car = result.rows[0];
+          res.status(200).send(car);
+        }else{
+          res.status(404).send();
+        }
+      });
+});
+
+/**
+ * POST one car
+ */
+
+app.post('/api/cars', (req, res) =>{
+    const pool = new Pool({
+        user: 'db_user',
+        host: 'database',
+        database: 'db_db',
+        password: 'db_password'
       })
-});*/
+      var uuid = uuidv4();
+      var name = req.body.name;
+      var fuelType = req.body.fuelType;
+      var year = req.body.year;
+
+      pool.query('INSERT INTO cars VALUES($1,$2,$3,$4)', [uuid, name, fuelType, year], (err, result) => {
+        var car = {
+          "id": uuid,
+          "name": name,
+          "fuelType": fuelType,
+          "year": year          
+        }
+
+        pool.end();
+        res.status(201).send(car);
+      })
+});
+
+/**
+ * DELETE one car
+ */
+app.delete('/api/cars/:id', (req, res) =>{
+  const pool = new Pool({
+      user: 'db_user',
+      host: 'database',
+      database: 'db_db',
+      password: 'db_password'
+    })
+
+    var id = req.params.id;
+
+    pool.query('DELETE FROM cars WHERE id = $1', [id], (err, result) => {
+      pool.end();
+      res.status(204).send();
+    })
+});
+
+/**
+ * PUT one car
+ */
+app.put('/api/cars/:id', (req, res) =>{
+  const pool = new Pool({
+      user: 'db_user',
+      host: 'database',
+      database: 'db_db',
+      password: 'db_password'
+    })
+
+    var id = req.params.id;
+    var name = req.body.name;
+    var fuelType = req.body.fuelType;
+    var year = req.body.year;
+
+    pool.query('UPDATE cars SET name = $1, fuelType = $2, year = $3 WHERE id = $4', [name, fuelType, year, id], (err, result) => {
+      pool.end();
+      if(err == null){
+        res.status(200).send();
+      }
+    })
+
+    res.status(404).send(err);
+});
 
 module.exports = app;
